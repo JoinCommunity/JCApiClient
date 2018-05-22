05923a3e4f0daca075dd397bf0d8380eefdfa10c
joselinoneto <joselinoneto@gmail.com>
2018-05-22 09:18:33 -0300

    Initial Commit

---
+++ JCApiClient/JCApiClient.h
@@ -0,0 +1,19 @@
+//
+//  JCApiClient.h
+//  JCApiClient
+//
+//  Created by Jose Lino Neto on 22/05/18.
+//  Copyright © 2018 Jose Lino Neto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JCApiClient.
+FOUNDATION_EXPORT double JCApiClientVersionNumber;
+
+//! Project version string for JCApiClient.
+FOUNDATION_EXPORT const unsigned char JCApiClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JCApiClient/PublicHeader.h>
+
+
