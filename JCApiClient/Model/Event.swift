68aeafd79ab60de2666740652fea41bf8ef2a3c8
joselinoneto <joselinoneto@gmail.com>
2018-05-22 09:59:28 -0300

    get using services, new proprieties

--- JCApiClient/Model/Event.swift
+++ JCApiClient/Model/Event.swift
@@ -8,11 +8,32 @@
 
 import Foundation
 
-public class Event  {
-    public var title: String
+public struct Event : Codable {
     
-    public init(title: String)
-    {
-        self.title = title
+    public var eventId: String
+    public var name: String
+    public var subject: String
+    public var speaker: String
+    public var room: String
+    public var createdAt: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case eventId = "_id"
+        case name = "name"
+        case subject = "subject"
+        case speaker = "speaker"
+        case room = "room"
+        case createdAt = "createdAt"
+    }
+    
+    //MARK: Local Api Service
+    private static let service: ServiceApi = ServiceApi<Event>()
+    
+    public static func getAllEvents(completeCall: @escaping([Event]?) -> (), errorCall:@escaping(String) -> ()) {
+        service.get(route: "", completeCall: { (returnData) in
+            completeCall(returnData)
+        }) { (errorMessage) in
+            errorCall(errorMessage)
+        }
     }
 }
