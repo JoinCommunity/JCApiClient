1a08059acfa33741e4a0f5120767a36886494c79
joselinoneto <joselinoneto@gmail.com>
2018-05-29 02:56:10 -0300

    basic io

--- JCApiClient/Services/ServiceIO.swift
+++ JCApiClient/Services/ServiceIO.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+public class ServiceIO {
+    public class func getDocumentDirectoryUrl() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    public class func fileExist(name: String?) -> Bool {
+        if let name = name {
+            let documentsDirectory = getDocumentDirectoryUrl()
+            let fileName = documentsDirectory.appendingPathComponent(name)
+            let fileManager = FileManager.default
+            let testFileExist = fileManager.fileExists(atPath: fileName.path)
+            
+            return testFileExist
+        }
+        else{
+            return false
+        }
+    }
+    
+    public class func getFileData(name: String?) -> Data? {
+        if fileExist(name: name) {
+            let fileName = getDocumentDirectoryUrl().appendingPathComponent(name!)
+            let data : Data? = try? Data(contentsOf: fileName)
+            return data
+        }
+        else {
+            return nil
+        }
+    }
+}
+
